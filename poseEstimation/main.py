69a42f61abc7203f20dc733fe0967520b092b78a
akatosh <akatosh@banggang>
2017-02-28 10:18:48 +0100

    add network architecture test

--- poseEstimation/main.py
+++ poseEstimation/main.py
@@ -34,23 +34,9 @@ if TEST:
     print(res)
 
 else:
-    iterations=3
-    total_error=0
-    id=1
-    name='network'+str(id)
-    if os.path.isfile(name+"_data.txt"):
-        os.remove(name+"_data.txt")
-    for _ in range(iterations):
-        network1=Network(train_data.shape[1],train_data.shape[2],train_labels.shape[1],name,networkFile='networks/network1.txt')
-        network1.trainNetwork(train_data,train_labels,valid_data,valid_labels,batchSize=10,epochs=100,learningRate=0.01,penalty=0.01)
-        error=network1.testNetwork(test_data,test_labels)
-        f=open(name+'_data.txt','a')
-        f.write(str(error)+'\n')
-        f.close()
-        print("CURRENT TEST ERROR : " + str(error))
-        total_error+=error
-        del network1
-    shapeData.formatData(name+"_data.txt")
-    total_error/=iterations
-    print(total_error)
+    network1=Network(train_data.shape[1],train_data.shape[2],train_labels.shape[1],'network',networkFile='networks/network1.txt')
+    network1.trainNetwork(train_data,train_labels,valid_data,valid_labels,batchSize=10,epochs=100,learningRate=0.01,penalty=0.01)
+    error=network1.testNetwork(test_data,test_labels)
+    print("CURRENT TEST ERROR : " + str(error))
+
 
