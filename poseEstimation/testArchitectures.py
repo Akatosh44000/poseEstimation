648771d65685f2e5188e0e5963cb9bc2d0b99b2b
akatosh <akatosh@irlivc98.polytech.univ-nantes.prive>
2017-03-02 11:36:28 +0100

    max_network and iteration number placed in argv test case deleted

--- poseEstimation/testArchitectures.py
+++ poseEstimation/testArchitectures.py
@@ -11,6 +11,10 @@ import batch_functions
 import os
 import shapeData
 from os.path import expanduser
+import sys
+
+max_network=int(sys.argv[1])
+iterations=int(sys.argv[2])
 
 home = expanduser("~")
 MODEL_PATH=home+'/DATASETS'
@@ -19,40 +23,29 @@ DATASET='MULTITUDE'
 MODEL_OBJECT='BREATHER'
 DIMENSION='SAMPLES'
 DEPTH=False
-TEST=False
 
 [train_data,train_labels]=datasetImportConv(MODEL_PATH,DATASET,MODEL_OBJECT,DIMENSION,'TRAIN',DEPTH)
 [valid_data,valid_labels]=datasetImportConv(MODEL_PATH,DATASET,MODEL_OBJECT,DIMENSION,'VALIDATION',DEPTH)
 [test_data,test_labels]=datasetImportConv(MODEL_PATH,DATASET,MODEL_OBJECT,DIMENSION,'TEST',DEPTH)
-#[test_dataV,test_labelsV]=datasetImportConv(MODEL_PATH,DATASET,MODEL_OBJECT,'VRAC','TEST',DEPTH)
 
-if TEST:
-    f=open('network1.dat','rb')
-    networkF=pickle.load(f)
-    test_data=batch_functions.normalize_batch(test_data)
-    res=networkF.testNetwork(test_data,test_labels)
-    print(res)
-
-else:
-    iterations=3
-    max_network=2
-    for id in range(1,max_network+1):
-        total_error=0
-        name='network'+str(id)
-        networkFile='networks/network'+str(id)+'.txt'
-        if os.path.isfile(name+"_data.txt"):
-            os.remove(name+"_data.txt")
-        for _ in range(iterations):
-            network1=Network(train_data.shape[1],train_data.shape[2],train_labels.shape[1],name,networkFile=networkFile)
-            network1.trainNetwork(train_data,train_labels,valid_data,valid_labels,batchSize=10,epochs=100,learningRate=0.01,penalty=0.01)
-            error=network1.testNetwork(test_data,test_labels)
-            f=open(name+'_data.txt','a')
-            f.write(str(error)+'\n')
-            f.close()
-            print("CURRENT TEST ERROR : " + str(error))
-            total_error+=error
-            del network1
-        shapeData.formatData(name+"_data.txt")
-        total_error/=iterations
-        print(total_error)
+iterations=3
+for id in range(1,max_network+1):
+    total_error=0
+    name='network'+str(id)
+    networkFile='networks/network'+str(id)+'.txt'
+    if os.path.isfile(name+"_data.txt"):
+        os.remove(name+"_data.txt")
+    for _ in range(iterations):
+        network1=Network(train_data.shape[1],train_data.shape[2],train_labels.shape[1],name,networkFile=networkFile)
+        network1.trainNetwork(train_data,train_labels,valid_data,valid_labels,batchSize=100,epochs=150,learningRate=0.01,penalty=0.01)
+        error=network1.testNetwork(test_data,test_labels)
+        f=open(name+'_data.txt','a')
+        f.write(str(error)+'\n')
+        f.close()
+        print("CURRENT TEST ERROR : " + str(error))
+        total_error+=error
+        del network1
+    shapeData.formatData(name+"_data.txt")
+    total_error/=iterations
+    print(total_error)
 
